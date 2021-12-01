9060fcdc84e4c1bb894734e8f49b64c38f1f3191
kckhafra <kckhafra@gmail.com>
2021-12-01 12:13:51 -0500

    inital

--- trace_txes.py
+++ trace_txes.py
@@ -50,7 +50,20 @@ class TXO:
         c = cls(tx_hash,n,amount,address,time)
         return c
     def get_inputs(self,d=1):
+        lists=[]
+        tx = rpc_connection.getrawtransaction(self.tx_hash,True)
+        #YOUR CODE HERE
+        amount = 0
+        address = ""
+        time = datetime.fromtimestamp(tx["time"])
+        for v in tx["vin"]:
+            vin_tx = self.from_tx_hash(v["txid"],v["vout"])
+            lists.append(vin_tx)
         
-        pass
+        self.inputs.append(lists)
+        d=d-1
+        if(d==0):
+            return self
+        self.get_inputs(self,d)
 
             
